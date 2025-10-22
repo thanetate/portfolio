89c77bad53c3c3d23400729fb0ac86ddb8db31f9
Tate, Thane <thanetate@my.unt.edu>
2025-10-21 22:33:06 -0500

    Footer - added href links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ function FooterComponent() {
 			<div className="text">Designed and developed by Thane Tate.</div>
 			<div className="leftright-container">
 				<div className="links-container">
-					<a>LinkedIn</a>
-					<a>Github</a>
-					<a>Email</a>
+					<a href="https://linkedin.com/in/thanetate">LinkedIn</a>
+					<a href="https://github.com/thanetate">Github</a>
+					<a href="mailto:thaneallantate@gmail.com">Email</a>
 				</div>
 				<div className="version-container">
 					<div className="version">v4.0.0</div>
