5d61cf1da84ca93c260af1be5c941e6a1375e72a
Tate, Thane <thanetate@my.unt.edu>
2025-10-26 16:30:51 -0500

    fix - styling projects page

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -1,15 +1,18 @@
+import { useParams } from "react-router-dom";
 import LineComponent from "../../components/line/line";
 import NavBarComponent from "../../components/navbar/navbar";
 import "./project.less";
 
 function Projectpage () {
+    const { projectname } = useParams();
+    const projecttitle = projectname ? projectname.charAt(0).toUpperCase() + projectname.slice(1) : "";
     return (
         <div className="projectpage-container">
             <div className="navbar-container">
 				<NavBarComponent />
 			</div>
             <div className="s-title">Project Details</div>
-            <div className="title">Mapo</div>
+            <div className="title">{projecttitle}</div>
             <LineComponent/>
         </div>
     )
