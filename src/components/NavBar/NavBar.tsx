2f6afc90d2c0d2bd35e0ac3793fff14f821bfe8d
Tate, Thane <thanetate@my.unt.edu>
2025-12-14 19:42:23 -0600

    Started working on desktop view

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,35 @@
 import "./navbar.less"
+import 'remixicon/fonts/remixicon.css';
 
 function NavBarComponent() {
-    return (
-        <div className="navbar-component-container">
-            <a href="/">Work</a>
-            <a href="/about">About</a>
-            <a href="/skills">Skills</a>
-        </div>
-    )
+	return (
+		<>
+			<div className="navbar-component-container">
+				<a href="/">
+					Work
+				</a>
+				<a href="/about">
+					About
+				</a>
+				<a href="/skills">
+					Skills
+				</a>
+			</div>
+			<div className="navbar-component-container2">
+				<a href="/">
+					<i className="ri-arrow-right-s-line"></i>
+					Work
+				</a>
+				<a href="/#About">
+					<i className="ri-arrow-right-s-line"></i>
+					About
+				</a>
+				<a href="/">
+					<i className="ri-arrow-right-s-line"></i>
+					Skills
+				</a>
+			</div>
+		</>
+	)
 }
 export default NavBarComponent;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,8 +6,24 @@ import FooterComponent from "../../components/footer/footer";
 import { motion } from "framer-motion";
 import "./home.less";
 import LineComponent from "../../components/line/line";
+import Aboutpage from "../about/about";
+import { useEffect, useState } from "react";
 
 function Homepage() {
+	const [showAbout, setShowAbout] = useState(false);
+
+	useEffect(() => {
+		const handleHashChange = () => {
+			setShowAbout(window.location.hash === "#About");
+		};
+
+		// Check on first load
+		handleHashChange();
+
+		// Listen for changes
+		window.addEventListener("hashchange", handleHashChange);
+		return () => window.removeEventListener("hashchange", handleHashChange);
+	}, []);
 	return (
 		<div className="homepage-container">
 			<motion.div
@@ -20,36 +36,42 @@ function Homepage() {
 					<NavBarComponent />
 				</div>
 
-				<div className="leftright-container">
-					<motion.div
-						initial={{ opacity: 0, y: 20 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ duration: 0.5, ease: "easeOut" }}
-						className="bg-white p-6 rounded-lg shadow-md"
-					>
-
-						<div className="head-container">
-							<HeadComponent />
+				<motion.div
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 0.5, ease: "easeOut" }}
+					className="bg-white p-6 rounded-lg shadow-md"
+				>
+					<div className="leftright-container">
+						<div className="leftside-container">
+							<div className="head-container">
+								<HeadComponent />
+							</div>
+							<div className="experience-container">
+								<ExperienceComponent />
+							</div>
 						</div>
 						<div className="rightside-container">
-							<a className="experience-container">
-								<ExperienceComponent />
-							</a>
-							<a className="projects-container">
-								<ProjectsComponent />
-							</a>
-							<a className="footer-container">
-								<LineComponent />
-								<FooterComponent />
-							</a>
+							{showAbout ? (
+								<Aboutpage />
+							) : (
+								<>
+									<a className="projects-container">
+										<ProjectsComponent />
+									</a>
+									<a className="footer-container">
+										<LineComponent />
+										<FooterComponent />
+									</a>
+
+								</>
+							)}
 						</div>
-					</motion.div>
-					{/* <div className="rightside-container">
-						<RightSide />
-					</div> */}
-				</div>
+					</div>
+				</motion.div>
 			</motion.div>
-		</div>
+
+		</div >
 	);
 }
 
