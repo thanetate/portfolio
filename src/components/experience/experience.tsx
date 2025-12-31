af40208219139f42f03ea7292de67ecf7c2b21ec
Tate, Thane <thanetate@my.unt.edu>
2025-12-30 18:35:10 -0600

    Hotfix - Experience section routing to non existing page, 404

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,36 +1,21 @@
-
-import { useEffect, useState } from "react";
 import "./experience.less";
 
 function ExperienceComponent() {
-	const [aboutHref, setAboutHref] = useState(
-		window.innerWidth >= 1000 ? "/#About" : "/about"
-	);
-
-	useEffect(() => {
-		const onResize = () => {
-			setAboutHref(window.innerWidth >= 1000 ? "/#About" : "/about");
-		};
-
-		window.addEventListener("resize", onResize);
-		return () => window.removeEventListener("resize", onResize);
-	}, []);
-
 	return (
 		<div className="experience-component-container">
 			<div className="currently">
 				<div className="title">Currently -</div>
-				<a href={aboutHref} className="experience">
+				<a href={"/#About"} className="experience">
 					Application Developer @ Fisher Investments
 				</a>
 			</div>
 
 			<div className="previously">
 				<div className="title">Previously -</div>
-				<a href={aboutHref} className="experience">
+				<a href={"/#About"} className="experience">
 					Application Developer Intern @ Fisher Investments
 				</a>
-				<a href={aboutHref} className="experience">
+				<a href={"/#About"} className="experience">
 					Freelance Web Developer
 				</a>
 			</div>
