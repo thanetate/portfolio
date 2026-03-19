aded87bbef47ff22ed8dee59d048e13a1ef698da
thanetate <thanetate1@gmail.com>
2026-03-19 16:08:33 -0500

    Navbar positioning and additions

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,14 +4,19 @@ import "./navbar.less";
 function NavBarComponent() {
   return (
     <div className="navbar-component-container">
-      <a href="/#">
-        <i className="ri-arrow-right-s-line"></i>
-        Work
+      <a href="/#" className="brand-link">
+        thanetate.dev
       </a>
-      <a href="/#About">
-        <i className="ri-arrow-right-s-line"></i>
-        About
-      </a>
+      <div className="nav-links">
+        <a href="/#">
+          <i className="ri-arrow-right-s-line"></i>
+          Home
+        </a>
+        <a href="/#About">
+          <i className="ri-arrow-right-s-line"></i>
+          About
+        </a>
+      </div>
     </div>
   );
 }
