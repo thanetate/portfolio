805ec0f0f3f9a3bc86dc8f6056f515105d568501
thanetate <thanetate1@gmail.com>
2026-03-19 17:03:53 -0500

    Changed the head component

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,12 +1,17 @@
 import "./head.less";
+import "remixicon/fonts/remixicon.css";
 
 function HeadComponent() {
   return (
     <div className="head-component-container">
       <div className="head-content">
         <div className="title">Thane Tate</div>
-        <div className="description">
-          Platform Engineer based in Dallas, TX.
+        <div className="description-line">
+          A recent grad with a degree in Computer Science.
+        </div>
+        <div className="location-line">
+          <i className="ri-map-pin-add-fill"></i>
+          <span>Dallas, TX</span>
         </div>
       </div>
     </div>
